56d6563e38d3766ea70729d382f11cf51984ce3e
patriciafraga123 <patriciafraga@hotmail.com>
2025-04-10 17:02:47 -0300

    Delete minha_api directory

--- minha_api/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.comentario import ComentarioSchema
-from schemas.filme import FilmeSchema, FilmeBuscaSchema, FilmeViewSchema, \
-                            ListagemFilmesSchema, FilmeDelSchema, FilmeUpdateSchema, \
-                            apresenta_filme, apresenta_filmes
-from schemas.error import ErrorSchema
--- minha_api/schemas/comentario.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ComentarioSchema(BaseModel):
-    """ Define como um novo comentário a ser inserido deve ser representado
-    """
-    filme_id: int = 1
-    texto: str = " Nada a comentar !"
